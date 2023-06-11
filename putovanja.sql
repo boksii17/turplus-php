45210468577bf4cece392a428ad23f98014ab4fe
Bojana Novakovic <novakovicc17@gmail.com>
2023-06-11 23:09:54 +0200

    izmena baze

--- putovanja.sql
+++ putovanja.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 11, 2023 at 10:33 PM
+-- Generation Time: Jun 11, 2023 at 11:09 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -20,6 +20,33 @@ SET time_zone = "+00:00";
 --
 -- Database: `putovanja`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `destinacije`
+--
+
+CREATE TABLE `destinacije` (
+  `id` int(11) NOT NULL,
+  `naziv` varchar(255) NOT NULL,
+  `datum` datetime NOT NULL,
+  `trajanje` varchar(255) NOT NULL,
+  `transport` varchar(255) NOT NULL,
+  `userid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `destinacije`
+--
+
+INSERT INTO `destinacije` (`id`, `naziv`, `datum`, `trajanje`, `transport`, `userid`) VALUES
+(66, 'Amsterdam', '2021-12-16 19:44:00', '6 dana', 'avion', 1),
+(67, 'Firenca', '2021-12-18 00:00:00', '8 dana', 'avion', 1),
+(69, 'Nis', '2021-12-14 02:03:00', '2 dana', 'autobus', 1),
+(70, 'Zlatibor', '2023-06-14 18:19:00', '8 dana', 'autobus', 1),
+(71, 'Barselona', '2023-06-15 22:24:00', '7 dana', 'avion', 1),
+(72, 'Madrid', '2023-08-10 23:21:00', '6 dana', 'autobus', 1);
 
 -- --------------------------------------------------------
 
@@ -50,6 +77,13 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
 --
 
 --
+-- Indexes for table `destinacije`
+--
+ALTER TABLE `destinacije`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userid` (`userid`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -60,10 +94,26 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `destinacije`
+--
+ALTER TABLE `destinacije`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `destinacije`
+--
+ALTER TABLE `destinacije`
+  ADD CONSTRAINT `destinacije_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
